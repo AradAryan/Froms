43fe84562f32f2eb626b0801b089b7d7eaae5ae5
Mohamad Javad A.Salmany <javadjavad541@gmail.com>
2019-10-11 23:50:02 +0330

    Change Value of TextBox From Another Form

---
+++ Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Froms
+{
+    public partial class Form2 : Form
+    {
+        private TextBox textBox;
+        public Form2(TextBox text)
+        {
+            InitializeComponent();
+            textBox = text;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            textBox.Text = "Hello";
+        }
+    }
+}
